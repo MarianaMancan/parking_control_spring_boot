7eae2a73ff9f682f335eaafb63ed4723a24d4701
MarianaMancan <80931170+MarianaMancan@users.noreply.github.com>
2023-04-03 14:49:51 -0300

    Add files via upload

---
+++ parking-control/src/main/java/repositores/ParkingSpotRepository.java
@@ -0,0 +1,17 @@
+package repositores;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import models.ParkingSpotModel;
+
+@Repository
+public interface ParkingSpotRepository extends  JpaRepository<ParkingSpotModel,UUID>{
+
+	boolean existsByLicensePlateCar(String licensePlateCar);
+	boolean existsByParkingSpotNumber(String parkingSpotNumber);
+	boolean existsByApartmentAndBlock(String apartment,String block);
+	
+}
